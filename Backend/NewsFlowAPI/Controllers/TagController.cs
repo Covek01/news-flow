b87ef418ecf27998e131e2bbddacae807dab17b0
Aleksandar Stojkovic <aleksandarstojkovic@elfak.rs>
2024-01-04 21:47:09 +0100

    Tag redis, GetTagByName

--- Backend/NewsFlowAPI/Controllers/TagController.cs
+++ Backend/NewsFlowAPI/Controllers/TagController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Mvc;
 using Neo4jClient;
 using NewsFlowAPI.Models;
 using NewsFlowAPI.Services;
 using StackExchange.Redis;
+using System.Text.Json;
 
 namespace NewsFlowAPI.Controllers
 {
@@ -41,6 +43,10 @@ namespace NewsFlowAPI.Controllers
                 .WithParam("tag", newTag)
                 .ExecuteWithoutResultsAsync();
 
+            var db = _redis.GetDatabase();
+            db.SetAdd("tags:nodes", JsonSerializer.Serialize(newTag));
+
+
             return Ok("Tag successfully added!");
         }
         //[Authorize]
@@ -62,12 +68,19 @@ namespace NewsFlowAPI.Controllers
             {
                 return NotFound($"Tag with Id:{id} not found");
             }
-
             await _neo4j.Cypher
                 .Match("(t:Tag)")
                 .Where((Tag t) => t.Id == id)
                 .DetachDelete("t")
                 .ExecuteWithoutResultsAsync();
+
+
+            Tag tempTag = new Tag();
+            tempTag.Id = id;
+            tempTag.Name=tag.ToList().First().Name;
+
+            var db = _redis.GetDatabase();
+            await db.SetRemoveAsync("tags:nodes", JsonSerializer.Serialize(tempTag));
 
             return Ok("Tag deleted");
 
@@ -98,6 +111,15 @@ namespace NewsFlowAPI.Controllers
                 .WithParam("name", name)
                 .ExecuteWithoutResultsAsync();
 
+            Tag tempTag = new Tag();
+            tempTag.Id = id;
+            tempTag.Name = tag.ToList().First().Name;
+
+            var db=_redis.GetDatabase();
+            await db.SetRemoveAsync("tags:nodes", JsonSerializer.Serialize(tempTag));
+            tempTag.Name = name;
+            await db.SetAddAsync("tags:nodes", JsonSerializer.Serialize(tempTag));
+
             return Ok("Tag updated");
         }
 
@@ -120,6 +142,45 @@ namespace NewsFlowAPI.Controllers
                 return NotFound($"Tag with Id:{id} not found");
             }
             return Ok(tag.ToList()[0].Name);
+        }
+
+        [HttpGet("getByName/{name}")]
+        public async Task<ActionResult> GetTagByName([FromRoute] string name)
+        {
+            var db = _redis.GetDatabase();
+            db.SetAdd("test", "aaa");
+            db.SetAdd("test", "aab");
+            db.SetAdd("test", "bac");
+            db.SetAdd("test", "bbb");
+
+            //var nest = db.SetScanAsync("tags:nodes", "*S*");
+            //var nest =
+            string setName = "tags:nodes";
+            string pattern = "*"+name+"*"; // Specify the pattern to match
+            int count = 10;
+            long cursor = 0;
+            RedisResult scanResult;
+            List<Tag> tagsMatched = new List<Tag>();
+            do
+            {
+                scanResult = db.Execute("SSCAN", setName, cursor, "MATCH", pattern, "COUNT", count);
+
+                var innerResults = (RedisResult[])scanResult;
+                cursor = (long)innerResults[0];
+
+                var matchingValues = (RedisValue[])innerResults[1];
+                foreach (var value in matchingValues)
+                {
+                    Tag tempTag = JsonSerializer.Deserialize<Tag>(value.ToString());
+                    tagsMatched.Add(tempTag);
+                }
+
+            } while (cursor != 0);
+
+
+
+
+            return Ok(tagsMatched);
         }
     }
 }
